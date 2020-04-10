a32f2d8ba73772da5a6c317aceee00ff098d6434
J. A. Hernández <joseanher81@gmail.com>
2020-04-10 12:48:44 +0200

    NavBar for game page

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -1,13 +1,19 @@
 import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "./../contexts/userContexts";
 import { loggedin } from "./../services/authService";
+import NavBar from "./../components/game/NavBar";
 
 const GamePage = () => {
   const { user } = useContext(UserContext);
 
   console.log("GAME " + JSON.stringify(user));
 
-  return <h1>GAME of {user.username}</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1>GAME of {user.username}</h1>
+    </>
+  );
 };
 
 export default GamePage;
