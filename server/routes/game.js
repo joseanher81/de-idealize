d17c437115ee5f371e23cea2cf418a1f44825f0e
J. A. Hernández <joseanher81@gmail.com>
2020-04-13 23:46:13 +0200

    Logic on game route

--- server/routes/game.js
+++ server/routes/game.js
@@ -2,60 +2,80 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Game = require("../models/game");
+const Chat = require("../models/chat");
 
 // New Game
 router.post("/game/new", async (req, res, next) => {
   console.log("Starting new game");
 
-  // Get current player
-  const { userid } = req.body;
-  const currentUser = await User.findById(userid);
-  //console.log("Usuario que vuelve: " + currentUser);
+  try {
+    // Get current player
+    const { userid } = req.body;
+    const currentUser = await User.findById(userid);
 
-  // Try to find a suitable match
-  const { lookingFor, minAge, maxAge, username, age, gender } = currentUser;
-  const matchedUser = await User.findOne({
-    $and: [
-      {
-        minAge: {
-          $lte: age,
+    // Try to find a suitable match
+    const { lookingFor, minAge, maxAge, username, age, gender } = currentUser;
+    const matchedUser = await User.findOne({
+      $and: [
+        {
+          minAge: {
+            $lte: age,
+          },
         },
-      },
-      {
-        maxAge: {
-          $gte: age,
+        {
+          maxAge: {
+            $gte: age,
+          },
         },
-      },
-      {
-        lookingFor: gender,
-      },
-      {
-        age: {
-          $lte: maxAge,
+        {
+          lookingFor: gender,
         },
-      },
-      {
-        age: {
-          $gte: minAge,
+        {
+          age: {
+            $lte: maxAge,
+          },
         },
-      },
-      {
-        gender: lookingFor,
-      },
-      {
-        currentGame: null,
-      },
-      {
-        username: {
-          $ne: username,
+        {
+          age: {
+            $gte: minAge,
+          },
         },
-      },
-    ],
-  });
+        {
+          gender: lookingFor,
+        },
+        {
+          currentGame: null,
+        },
+        {
+          username: {
+            $ne: username,
+          },
+        },
+      ],
+    });
 
-  console.log("Usuario encontrado: " + matchedUser);
+    console.log("Usuario encontrado: " + matchedUser);
 
-  // Create game
+    // Create game
+    const newChat = await Chat.create({});
+
+    const newGame = await Game.create({
+      playerTurn: userid,
+      chat: newChat,
+      playerA: currentUser,
+      playerB: matchedUser,
+    });
+
+    // Add game to players
+    currentUser.currentGame = newGame;
+    currentUser.save();
+    matchedUser.currentGame = newGame;
+    matchedUser.save();
+
+    res.json({ status: "ok", game: newGame.toJSON() });
+  } catch (error) {
+    console.log("ERROR CREATING NEW GAME " + error);
+  }
 });
 
 module.exports = router;
