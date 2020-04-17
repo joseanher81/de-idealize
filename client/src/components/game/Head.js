8840cfdfa895139d74f5d6740863c55c88736b10
J. A. Hernández <joseanher81@gmail.com>
2020-04-17 20:24:49 +0200

    Changed icons to pics and align them

--- client/src/components/game/Head.js
+++ client/src/components/game/Head.js
@@ -4,6 +4,7 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   head: {
@@ -17,6 +18,13 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "7em",
     color: "#FF8BA7",
   },
+  image: {
+    width: theme.spacing(12),
+    height: theme.spacing(12),
+  },
+  item: {
+    marginTop: "1em",
+  },
 }));
 
 const Head = (props) => {
@@ -29,14 +37,26 @@ const Head = (props) => {
         {rival && rival.username}
       </Typography>
       <Grid container>
-        <Grid item xs={4}>
-          <AccountCircleIcon className={classes.icon} />
+        <Grid item xs={4} className={classes.item} align="center">
+          <Avatar
+            alt="Remy Sharp"
+            src="https://i.pinimg.com/originals/33/4f/5b/334f5bce0d22bc296a315839308d24a0.jpg"
+            className={classes.image}
+          />
         </Grid>
-        <Grid item xs={4}>
-          <AccountCircleIcon className={classes.icon} />
+        <Grid item xs={4} className={classes.item} align="center">
+          <Avatar
+            alt="Remy Sharp"
+            src="https://i.pinimg.com/originals/33/4f/5b/334f5bce0d22bc296a315839308d24a0.jpg"
+            className={classes.image}
+          />
         </Grid>
-        <Grid item xs={4}>
-          <AccountCircleIcon className={classes.icon} />
+        <Grid item xs={4} className={classes.item} align="center">
+          <Avatar
+            alt="Remy Sharp"
+            src="https://i.pinimg.com/originals/33/4f/5b/334f5bce0d22bc296a315839308d24a0.jpg"
+            className={classes.image}
+          />
         </Grid>
       </Grid>
     </Container>
