149e5f8f9eff4a04f309d66d61adefef96cdb74b
J. A. Hernández <joseanher81@gmail.com>
2020-04-18 18:13:15 +0200

    Progress bar first styling

--- client/src/components/game/Timer.js
+++ client/src/components/game/Timer.js
@@ -2,23 +2,32 @@ import React, { useRef, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  box: {
+    width: "100px",
+    height: "100px",
+    margin: "auto",
+  },
+}));
 
 const Timer = (props) => {
   const classes = useStyles();
   const refTimer = useRef();
   const { messages } = props;
+  const percentage = 66;
 
   useEffect(() => {
-    refTimer.current.innerHTML = "Testing";
+    //refTimer.current.innerHTML = "Testing";
     let currentMilis = 0;
     let currentTime = 599;
 
     const intervalId = setInterval(function () {
       currentMilis++;
       if (currentMilis % 100 == 0) currentTime--;
-      refTimer.current.innerHTML = `${getMinutes()} : ${getSeconds()}`;
+      // refTimer.current.innerHTML = `${getMinutes()} : ${getSeconds()}`;
     }, 10);
 
     const getMinutes = () => Math.floor(currentTime / 60);
@@ -30,10 +39,34 @@ const Timer = (props) => {
   }, [messages]);
 
   return (
-    <div>
-      <p>Timer</p>
-      <p ref={refTimer}></p>
-    </div>
+    <Box className={classes.box}>
+      <CircularProgressbar
+        value={percentage}
+        text={`${percentage}%`}
+        styles={buildStyles({
+          // Rotation of path and trail, in number of turns (0-1)
+          rotation: 0.25,
+
+          // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+          strokeLinecap: "butt",
+
+          // Text size
+          textSize: "16px",
+
+          // How long animation takes to go from one percentage to another, in seconds
+          pathTransitionDuration: 0.5,
+
+          // Can specify path transition in more detail, or remove it entirely
+          // pathTransition: 'none',
+
+          // Colors
+          pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+          textColor: "#ff8ba7",
+          trailColor: "#d6d6d6",
+          backgroundColor: "#3e98c7",
+        })}
+      />
+    </Box>
   );
 };
 
